8daa79d3cffcfed8c6498d640327362f98eb3e50
sadick-koyo <koyosadick@gmail.com>
2026-05-01 23:18:02 +0300

    Create construction.php

---
+++ modules/construction.php
@@ -0,0 +1,31 @@
+<?php include "../config.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Construction Module</title>
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<div class="main">
+<h1>Construction Department</h1>
+
+<div class="cards">
+<div class="card">
+<h3>Active Sites</h3>
+<p>06</p>
+</div>
+
+<div class="card">
+<h3>Workers</h3>
+<p>85</p>
+</div>
+
+<div class="card">
+<h3>Materials</h3>
+<p>142</p>
+</div>
+</div>
+
+</div>
+</body>
+</html>

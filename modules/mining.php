46b731febd5d7e61dd39c26b81098d4971bffe0d
sadick-koyo <koyosadick@gmail.com>
2026-05-01 23:18:56 +0300

    Create mining.php

---
+++ modules/mining.php
@@ -0,0 +1,31 @@
+<?php include "../config.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mining Module</title>
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<div class="main">
+<h1>Mining Department</h1>
+
+<div class="cards">
+<div class="card">
+<h3>Mining Sites</h3>
+<p>04</p>
+</div>
+
+<div class="card">
+<h3>Machines</h3>
+<p>16</p>
+</div>
+
+<div class="card">
+<h3>Ore Output</h3>
+<p>220T</p>
+</div>
+</div>
+
+</div>
+</body>
+</html>

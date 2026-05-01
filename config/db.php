0d6d214b6517018e83274c87b775a89da3d57ddd
sadick-koyo <koyosadick@gmail.com>
2026-05-01 22:31:25 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,1 +1,13 @@
 
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "engineering_system";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>

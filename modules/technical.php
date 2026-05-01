3b3459e7e7f4dc04afe6d879d68dc239104e8d8e
sadick-koyo <koyosadick@gmail.com>
2026-05-01 23:20:45 +0300

    Create technical.php

---
+++ modules/technical.php
@@ -0,0 +1,31 @@
+<?php include "../config.php"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Metal Works</title>
+<link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+<div class="main">
+<h1>Metal Fabrication</h1>
+
+<div class="cards">
+<div class="card">
+<h3>Orders</h3>
+<p>19</p>
+</div>
+
+<div class="card">
+<h3>Machines</h3>
+<p>07</p>
+</div>
+
+<div class="card">
+<h3>Completed Jobs</h3>
+<p>11</p>
+</div>
+</div>
+
+</div>
+</body>
+</html>
